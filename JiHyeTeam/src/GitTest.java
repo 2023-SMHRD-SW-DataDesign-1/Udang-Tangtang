ff3395e8bcbd00df0ef8f404ae8efca89f883211
Smhrdd <Smhrdd@DESKTOP-9Q5B97N>
2023-05-26 11:26:23 +0900

    230526 이윤호 push , pull 설명

--- JiHyeTeam/src/GitTest.java
+++ JiHyeTeam/src/GitTest.java
@@ -6,7 +6,18 @@ public class GitTest {
 		System.out.println("박송이");
 		System.out.println("김지혜");
 
-           
+            
+//		클래스 우클릭 >> team >> commit (커밋창 오픈)
+//		  원하는 코드 실행(ctrl + f11)후   
+//		  하단 unstaged changes 에서 초록색 +버튼 눌러서
+//		  바로 아래 staged changes에 추가한 후  
+//		  commit message에 날짜, 이름, 내용 추가하고 우측 하단 commit 버튼 누르기! 
+//		   >> 클래스파일 우클릭 > team > push to origin 눌러서 push 완료.
+		
+//		클래스 우클릭 >> team >> push(2개 있는데 위에꺼, 물음표 아님.)
+		
+		  
+		
 
 	}// main
 
