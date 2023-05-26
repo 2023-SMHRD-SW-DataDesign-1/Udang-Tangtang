a83eeb5939a850fc3e7d6ad8eb251c5636aa5079
smhrd <smhrd@DESKTOP-0SQ7L5K>
2023-05-26 11:19:51 +0900

    230526 김지혜 출력문

--- JiHyeTeam/src/GitTest.java
+++ JiHyeTeam/src/GitTest.java
@@ -3,7 +3,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("이윤호");
-           
+		System.out.println("박송이");
+        System.out.println("김지혜");   
 	}// main
 
 }// class
