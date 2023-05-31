1c5a5064c01f01ad440bb2bb2d3124f2fec3efee
smhrd <smhrd@DESKTOP-BACAOUQ>
2023-05-31 09:48:21 +0900

    20230531 박송이 타이머수정

--- JiHyeTeam/src/Controller/Timecount.java
+++ JiHyeTeam/src/Controller/Timecount.java
@@ -10,21 +10,23 @@ public class Timecount {
 		Timer timer_m = new Timer();
 		TimerTask m_task = new TimerTask() {
 			int count = 1;
+			int score = 180;
 
 			@Override
 			public void run() {
-				if (count <= 5) {
+				if (count <= 60) {
 					System.out.println();
-					System.out.println("[카운트다운" + count + "초]");
+					System.out.print("[점수" + score + "] ");
 					count++;
+					score-=3;
+					
 				} else {
 					timer_m.cancel();
-					System.out.println("카운트가 종료되었습니다.");
+					System.out.println("시간이 종료되었습니다.");
 				}
 			}
 		};
-		timer_m.schedule(m_task, 10000, 1000); // 10초 뒤 카운트다운 실행
-
+		timer_m.schedule(m_task, 1000,1000); // 노래시작과 동시에 카운트다운시작
 	}
 
 } // class
