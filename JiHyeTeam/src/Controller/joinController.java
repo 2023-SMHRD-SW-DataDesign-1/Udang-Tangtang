9f1fbf75b269d40e04d633cad5454f5db8d317e2
smhrd <smhrd@DESKTOP-0SQ7L5K>
2023-06-01 10:54:51 +0900

    230601 김지혜 joincon수정

--- JiHyeTeam/src/Controller/joinController.java
+++ JiHyeTeam/src/Controller/joinController.java
@@ -8,15 +8,15 @@ public class joinController {
 
 	// 사용자가 입력한 데이터 확인
 	public String Join(memberDTO memdto) {
-		System.out.println("id 는 " + memdto.getId());
-		System.out.println("pw 는 " + memdto.getPw());
+		System.out.println("id : " + memdto.getId()+"pw : " + memdto.getPw());
+		System.out.println("회원가입이 완료되었습니다");
 
 		memberDAO dao = new memberDAO();
 		int cnt = dao.insertMember(memdto);
 
 		String result = null;
 		if (cnt > 0) {
-			System.out.println("회원가입 성공");
+			System.out.println("♡  "+memdto.getId()+"님 환영합니다  ♡");
 			result = "성공";
 		} else {
 			System.out.println("회원가입 실패");
